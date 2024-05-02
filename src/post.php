c1ce87dec8ebd0280ecee068de3fb2530ae7035a
JhairM16 <192H17117@alumno.ujat.mx>
2024-05-02 08:01:05 -0600

    carpeta archivos

--- src/post.php
+++ src/post.php
@@ -7,6 +7,15 @@ if ($data && isset($data['nombre']) && isset($data['emociones'])) {
     // Nombre del archivo JSON
     $nombreArchivo = $data['nombre'];
 
+    // Crear una carpeta si no existe
+    $carpeta = 'archivos_json';
+    if (!file_exists($carpeta)) {
+        mkdir($carpeta, 0777, true);
+    }
+
+    // Ruta completa del archivo JSON
+    $rutaArchivo = $carpeta . '/' . $nombreArchivo;
+
     // Datos de emociones
     $emociones = $data['emociones'];
 
@@ -14,14 +23,15 @@ if ($data && isset($data['nombre']) && isset($data['emociones'])) {
     $jsonString = json_encode($emociones, JSON_PRETTY_PRINT);
 
     // Guardar en un archivo JSON
-    $archivo = fopen($nombreArchivo, 'w');
+    $archivo = fopen($rutaArchivo, 'w');
     fwrite($archivo, $jsonString);
     fclose($archivo);
 
     // Enviar una respuesta al cliente
-    echo 'Datos guardados correctamente en el archivo ' . $nombreArchivo;
+    echo 'Datos guardados correctamente en el archivo ' . $rutaArchivo;
 } else {
     // Si los datos no se recibieron correctamente
     echo 'Error: No se recibieron los datos correctamente.';
 }
 ?>
+
